77aed051b03da54c025e4179b558b7ad727eec42
Andreas Svensson <andreas.svensson@iths.se>
2022-11-19 08:56:26 +0100

    added filter script

---
+++ filter.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Script for filtering DataFrame based on selected buttons in dashboard
+
+
+def filter_data(data, all_buttons):
+    pass
+    # filter data based on button1, button2, button3...
+    # return data
+
+
+def set_default_filters(dropdown_selection):
+    pass
+    # return setting1, setting2, setting3...
