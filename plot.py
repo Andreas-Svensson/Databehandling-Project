818c2a90c371efc09077379aa5fbc66fd73848c9
Andreas Svensson <andreas.svensson@iths.se>
2022-11-19 08:56:47 +0100

    added plotting script

---
+++ plot.py
@@ -0,0 +1,20 @@
+import plotly_express as px
+
+# Script for plotting DataFrame based on dropdown selection
+
+
+def plot_data(data, dropdown_selection):
+
+    if dropdown_selection == "A":
+        fig = ""  # graph A
+
+    elif dropdown_selection == "B":
+        fig = ""  # graph B
+
+    elif dropdown_selection == "C":
+        fig = ""  # graph C
+
+    elif dropdown_selection == "D":
+        fig = ""  # graph D
+
+    return fig
